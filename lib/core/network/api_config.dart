ea214ba2ee0e4e6f61d81b1791896eca97d83734
hananmalik21 <hananmalik719@gmail.com>
2025-12-30 11:54:07 +0500

    url updated

--- lib/core/network/api_config.dart
+++ lib/core/network/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   // static const String baseUrl = 'http://localhost:3000';
-  static const String baseUrl = 'https://digift-hr-system-backend.onrender.com';
+  static const String baseUrl = 'https://digift-hr-system-backend-48wi.onrender.com/';
   static const Duration connectTimeout = Duration(seconds: 30);
   static const Duration receiveTimeout = Duration(seconds: 30);
 }
