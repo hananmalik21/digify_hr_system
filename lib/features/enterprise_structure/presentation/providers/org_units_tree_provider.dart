68338ac4bf1415d2055d8ff4b7cad57dee2539bb
Affan Saleem <m.affansalim@gmail.com>
2026-02-21 21:52:06 +0500

    Update
    Tree node cleanup

--- lib/features/enterprise_structure/presentation/providers/org_units_tree_provider.dart
+++ lib/features/enterprise_structure/presentation/providers/org_units_tree_provider.dart
@@ -1,24 +1,81 @@
-import 'package:digify_hr_system/core/network/exceptions.dart';
 import 'package:digify_hr_system/features/enterprise_structure/domain/models/org_unit_tree.dart';
 import 'package:digify_hr_system/features/enterprise_structure/domain/usecases/get_org_units_tree_usecase.dart';
 import 'package:digify_hr_system/features/enterprise_structure/presentation/providers/structure_level_providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-/// Provider for get org units tree use case
+class OrgUnitsTreeState {
+  final AsyncValue<OrgUnitTree> tree;
+  final Set<String> expandedNodes;
+
+  OrgUnitsTreeState({required this.tree, this.expandedNodes = const {}});
+
+  OrgUnitsTreeState copyWith({AsyncValue<OrgUnitTree>? tree, Set<String>? expandedNodes}) {
+    return OrgUnitsTreeState(tree: tree ?? this.tree, expandedNodes: expandedNodes ?? this.expandedNodes);
+  }
+}
+
 final getOrgUnitsTreeUseCaseProvider = Provider<GetOrgUnitsTreeUseCase>((ref) {
   final repository = ref.watch(orgUnitRepositoryProvider);
   return GetOrgUnitsTreeUseCase(repository: repository);
 });
 
-/// Provider for org units tree
-final orgUnitsTreeProvider = FutureProvider.autoDispose<OrgUnitTree>((ref) async {
-  final useCase = ref.watch(getOrgUnitsTreeUseCaseProvider);
-  
-  try {
-    return await useCase();
-  } on AppException catch (e) {
-    throw Exception(e.message);
-  } catch (e) {
-    throw Exception('Failed to load org units tree: ${e.toString()}');
+class OrgUnitsTreeNotifier extends AutoDisposeNotifier<OrgUnitsTreeState> {
+  @override
+  OrgUnitsTreeState build() {
+    _loadTree();
+    return OrgUnitsTreeState(tree: const AsyncValue.loading());
   }
-});
+
+  Future<void> _loadTree() async {
+    final useCase = ref.watch(getOrgUnitsTreeUseCaseProvider);
+    try {
+      final tree = await useCase();
+      state = state.copyWith(tree: AsyncValue.data(tree));
+    } catch (e, stack) {
+      state = state.copyWith(tree: AsyncValue.error(e, stack));
+    }
+  }
+
+  void toggleNode(String nodeId) {
+    final expanded = Set<String>.from(state.expandedNodes);
+    if (expanded.contains(nodeId)) {
+      expanded.remove(nodeId);
+    } else {
+      expanded.add(nodeId);
+    }
+    state = state.copyWith(expandedNodes: expanded);
+  }
+
+  void expandAll() {
+    final tree = state.tree.valueOrNull;
+    if (tree == null) return;
+
+    final allNodes = <String>{};
+    void addNode(OrgUnitTreeNode node) {
+      if (node.children.isNotEmpty) {
+        allNodes.add(node.orgUnitId);
+        for (final child in node.children) {
+          addNode(child);
+        }
+      }
+    }
+
+    for (final node in tree.tree) {
+      addNode(node);
+    }
+    state = state.copyWith(expandedNodes: allNodes);
+  }
+
+  void collapseAll() {
+    state = state.copyWith(expandedNodes: {});
+  }
+
+  Future<void> refresh() async {
+    state = state.copyWith(tree: const AsyncValue.loading());
+    await _loadTree();
+  }
+}
+
+final orgUnitsTreeProvider = NotifierProvider.autoDispose<OrgUnitsTreeNotifier, OrgUnitsTreeState>(
+  OrgUnitsTreeNotifier.new,
+);
--- lib/features/enterprise_structure/presentation/screens/manage_component_values/widgets/views/component_values_tree_view.dart
+++ lib/features/enterprise_structure/presentation/screens/manage_component_values/widgets/views/component_values_tree_view.dart
@@ -1,6 +1,4 @@
-import 'package:digify_hr_system/core/localization/l10n/app_localizations.dart';
-import 'package:digify_hr_system/core/theme/theme_extensions.dart';
-import 'package:digify_hr_system/features/enterprise_structure/presentation/widgets/manage_enterprise_structure_widgets/org_units_tree_widget.dart';
+import 'package:digify_hr_system/features/enterprise_structure/presentation/widgets/manage_enterprise_structure_widgets/org_tree/org_units_tree_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,9 +7,6 @@ class ComponentValuesTreeView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final localizations = AppLocalizations.of(context)!;
-    final isDark = context.isDark;
-
-    return OrgUnitsTreeWidget(localizations: localizations, isDark: isDark);
+    return const OrgUnitsTreeWidget();
   }
 }
